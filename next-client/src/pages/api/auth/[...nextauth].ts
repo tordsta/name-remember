6694d05b18a1c52e5e43811e3ca39dd9aa704488
Tord Standnes <tord.standnes@gmail.com>
2023-04-19 18:23:03 +0200

    added facebook auth method

--- next-client/src/pages/api/auth/[...nextauth].ts
+++ next-client/src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,20 @@
 import NextAuth from "next-auth";
+//import AppleProvider from "next-auth/providers/apple";
+import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
   secret: process.env.NEXT_AUTH ?? "",
   providers: [
+    // AppleProvider({
+    //   clientId: process.env.APPLE_ID ?? "",
+    //   clientSecret: process.env.APPLE_SECRET ?? "",
+    // }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID ?? "",
+      clientSecret: process.env.FACEBOOK_SECRET ?? "",
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? "",
       clientSecret: process.env.GOOGLE_SECRET ?? "",
