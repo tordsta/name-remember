5baecb04c9f6bc4583c314401c4a11ac1d6f162e
Tord Standnes <tord.standnes@gmail.com>
2023-04-19 20:56:54 +0200

    slack auth added

--- next-client/src/pages/api/auth/[...nextauth].ts
+++ next-client/src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
+import SlackProvider from "next-auth/providers/slack";
 
 export const authOptions = {
   secret: process.env.NEXT_AUTH ?? "",
@@ -29,6 +30,10 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    SlackProvider({
+      clientId: process.env.SLACK_ID as string,
+      clientSecret: process.env.SLACK_SECRET as string,
     }),
   ],
 };
