6de5007b47b251af00d59f7c4af5302df19b2ce5
Tord Standnes <tord.standnes@gmail.com>
2023-04-19 17:58:17 +0200

    added google auth method

--- next-client/src/pages/api/auth/[...nextauth].ts
+++ next-client/src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,21 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
   secret: process.env.NEXT_AUTH ?? "",
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
