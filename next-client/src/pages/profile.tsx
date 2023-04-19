719c8c8ca825202a9bf427c414c44f9e3ecb4d21
Tord Standnes <tord.standnes@gmail.com>
2023-04-19 16:51:44 +0200

    fix build issues

--- next-client/src/pages/profile.tsx
+++ next-client/src/pages/profile.tsx
@@ -1,15 +1,4 @@
-import { useEffect } from "react";
-import { useAuthContext } from "../utils/authContext";
-import { useRouter } from "next/navigation";
-
 export default function Profile() {
-  const user = useAuthContext();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (user == null) router.push("/");
-  }, [user, router]);
-
   return (
     <div>
       <p>protected</p>
---
+++ next-client/src/pages/signup.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <div>
+      <p>Sign Up</p>
+    </div>
+  );
+}
