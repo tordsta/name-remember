78540a9c291fada5bf4f99bb80f10043d739d9e9
Tord Standnes <tord.standnes@gmail.com>
2023-05-21 21:16:55 +0200

    debuged mail sender

--- next-client/src/pages/api/mail/hourlyMailSender.ts
+++ next-client/src/pages/api/mail/hourlyMailSender.ts
@@ -1,6 +1,8 @@
+import { sql } from "@vercel/postgres";
 import { NextApiRequest, NextApiResponse } from "next";
+import postmark from "postmark";
 
-type Lists = {
+type List = {
   id: string;
   createdAt: string;
   updatedAt: string;
@@ -10,55 +12,67 @@ type Lists = {
   trigger_frequency: string | null;
 };
 
+var client = new postmark.ServerClient(process.env.POSTMARK_API_KEY as string);
+
+const sendEmail = (list: List) => {
+  client.sendEmail({
+    From: "reminders@nameremember.com",
+    To: "test@nameremember.com",
+    Subject: "Test",
+    TextBody: `Hello from Postmark! List id: ${list.id}`,
+  });
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("hourlyMailSender");
-
-  var postmark = require("postmark");
-  var client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
-  const sendEmail = (list: Lists) => {
-    client.sendEmail({
-      From: "reminders@nameremember.com",
-      To: list.owner_id,
-      Subject: "Test",
-      TextBody: `Hello from Postmark! List id: ${list.id}`,
-    });
-  };
+  console.log("Starting hourlyMailSender");
 
   let rows;
   try {
-    const res = await client.query(`
+    //join user on people_lists.owner_id = user.id, to get email
+    const res = await sql`
         SELECT * FROM people_lists
         WHERE reminder_trigger_time <= NOW()
-    `);
+    `;
     rows = res.rows;
   } catch (error) {
-    console.log(error);
-    res.status(500).json("Error: " + error);
+    console.error("Failed to fetch people_lists", error);
+    res
+      .status(500)
+      .json({ error: "Failed to fetch people_lists", details: error });
     return;
   }
   console.log("res ", rows);
 
   for (let i = 0; i < rows.length; i++) {
     const list = rows[i];
-    sendEmail(list);
-  }
-
-  for (let i = 0; i < rows.length; i++) {
-    const list = rows[i];
     try {
-      const res = await client.query(`
-        UPDATE people_lists
-        SET reminder_trigger_time = NOW() + INTERVAL ${list.trigger_frequency}
-        WHERE id = ${list.id}
-        `);
+      sendEmail(list as List);
     } catch (error) {
-      console.log(error);
+      console.error("Error while sending email", error, "TO: ", list.owner_id);
       continue;
     }
   }
 
-  res.status(200).end("Hello Cron!");
+  for (let i = 0; i < rows.length; i++) {
+    console.log("updating reminder_trigger_time", rows[i]);
+    const list = rows[i];
+    try {
+      const res = await sql`
+        UPDATE people_lists
+        SET reminder_trigger_time = NOW() + INTERVAL '${list.trigger_frequency}'
+        WHERE id = ${list.id}
+        `;
+    } catch (error) {
+      console.error(
+        `Failed to update reminder_trigger_time for list id: ${list.id}`,
+        error
+      );
+      continue;
+    }
+  }
+
+  res.status(200).end("Emails sent!");
 }
