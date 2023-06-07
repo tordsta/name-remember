097f7f55894ba06d4eece7934c1142fd8eb235fb
Tord Standnes <tord.standnes@gmail.com>
2023-06-07 15:34:06 +0200

    trigger deploy

--- next-client/src/pages/_app.tsx
+++ next-client/src/pages/_app.tsx
@@ -17,6 +17,7 @@ export default function App({
   pageProps: any;
 }) {
   const router = useRouter();
+
   useEffect(() => {
     router.events.on("routeChangeComplete", pageview);
     return () => {
