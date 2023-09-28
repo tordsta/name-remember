6b25d21542d9057c71ec63b9ce56a21bc7d9ad3e
Tord Standnes <tord.standnes@gmail.com>
2023-09-28 20:45:59 +0200

    bugfix stripe key and subscription bug

--- next-client/src/lib/stripe.ts
+++ next-client/src/lib/stripe.ts
@@ -7,5 +7,5 @@ export const stripeServer = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 export const stripeClientPromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+  `${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!}`
 );
