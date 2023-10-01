619633e55f87926fd2de967847b1a82742c3eefe
Tord Standnes <tord.standnes@gmail.com>
2023-10-01 14:02:04 +0200

    added public env vars to docker file for correct build config

--- next-client/src/lib/stripe.ts
+++ next-client/src/lib/stripe.ts
@@ -1,11 +1,11 @@
 import Stripe from "stripe";
 import { loadStripe } from "@stripe/stripe-js";
 
-export const stripeServer = new Stripe(`${process.env.STRIPE_SECRET_KEY!}`, {
+export const stripeServer = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-08-16",
   typescript: true,
 });
 
 export const stripeClientPromise = loadStripe(
-  `${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!}`
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
 );
