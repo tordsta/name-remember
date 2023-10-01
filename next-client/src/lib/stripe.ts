bbe5bff361420705128cba5617101181c26e54ef
Tord Standnes <tord.standnes@gmail.com>
2023-10-01 12:11:30 +0200

    bugfix build step

--- next-client/src/lib/stripe.ts
+++ next-client/src/lib/stripe.ts
@@ -1,9 +1,6 @@
 import Stripe from "stripe";
 import { loadStripe } from "@stripe/stripe-js";
 
-if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
-  throw new Error("Missing Stripe Publishable Key");
-
 export const stripeServer = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-08-16",
   typescript: true,
