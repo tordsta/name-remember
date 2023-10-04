36d76abc40df9e44ed3b38e451387388b6f03cc3
Tord Standnes <tord.standnes@gmail.com>
2023-10-04 21:47:44 +0200

    refactor index page remove divs

--- next-client/src/pages/index.tsx
+++ next-client/src/pages/index.tsx
@@ -5,7 +5,6 @@ import LegalInfo from "@/components/LegalInfo";
 import { useEffect } from "react";
 import { trackAmplitudeData } from "@/lib/amplitude";
 import { useSession } from "next-auth/react";
-import { FramedButton } from "@/components/Button";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -16,7 +15,9 @@ export default function Home() {
   return (
     <Layout nav={false} auth={false}>
       <div className="flex flex-row min-h-[100vh]">
+        {/* Whole page on mobile, left side on desktop. */}
         <div className="flex flex-col justify-evenly md:justify-center items-center py-8 md:py-0 border-0 md:border border-black w-full md:w-[50vw] h-full">
+          {/* Hidden on desktop */}
           <h1 className="block md:hidden text-3xl">Name Remember</h1>
           <div className="relative w-full h-full mt-4 mx-4 max-w-[250px] md:max-w-[384px] min-h-[250px] max-h-[250px] md:max-h-[384px]">
             <Image
@@ -41,29 +42,25 @@ export default function Home() {
             <li>Get reminders and memorize</li>
             <li>Never forget a name again</li>
           </ol>
-          {/* mobile version */}
+          {/* Hidden on desktop */}
           <div className="flex flex-col justify-center items-center md:hidden mt-8">
             <LoginButton />
             <LegalInfo />
           </div>
         </div>
-        {/* desktop version */}
-        <div className="hidden md:flex flex-col items-center bg-white w-[50vw] h-full">
+        {/* Right side on desktop, hidden on mobile */}
+        <div className="hidden md:flex flex-col bg-white w-[50vw] h-full">
           <h1 className="text-4xl ml-auto p-16">Name Remember</h1>
-          <div className="flex flex-col flex-grow justify-center items-center">
-            {/* <LoginButton /> */}
-            <div className="flex flex-col justify-center items-center">
-              <p className="text-2xl md:text-3xl mx-auto mb-4 text-center">
-                Get started today,
-                <br /> it&apos;s free.
-              </p>
-              <LoginButton loginText="Create new account" />
-              <p className="text-xl mx-auto my-2 text-center">or</p>
-              <LoginButton />
-            </div>
+          <div className="flex flex-col items-center justify-center h-full pb-40">
+            <p className="text-2xl md:text-3xl mx-auto mb-4 text-center">
+              Get started today,
+              <br /> it&apos;s free.
+            </p>
+            <LoginButton loginText="Create new account" />
+            <p className="text-xl mx-auto my-2 text-center">or</p>
+            <LoginButton />
             <LegalInfo />
           </div>
-          <div className="p-20" />
         </div>
       </div>
     </Layout>
